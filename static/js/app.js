57008a23b60d05b48c6d3589cce9e3da49f9be60
Ben Lewis <99ben.lewis@gmail.com>
2022-09-01 12:31:02 +0800

    add signup tab to login modal

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,28 @@
 console.log("Hello")
+
+$(() => {
+    $(".login-button").click(() => {
+        $("#signupModalLabel").removeClass("active")
+        $("#modalSignupForm").hide()   
+        $("#loginModalLabel").addClass("active")
+        $("#modalLoginForm").show()
+    })
+})
+
+$(() => {
+    $("#loginModalLabel").click(() => {
+        $("#signupModalLabel").removeClass("active")
+        $("#modalSignupForm").hide()
+        $("#loginModalLabel").addClass("active")
+        $("#modalLoginForm").show()
+    })
+})
+
+$(() => {
+    $("#signupModalLabel").click(() => {
+        $("#loginModalLabel").removeClass("active")
+        $("#modalLoginForm").hide()
+        $("#signupModalLabel").addClass("active")
+        $("#modalSignupForm").show()
+    })
+})
